fbf3cc50434f8b5889098ff7e8a47c6fabb9d043
Deepanjali <89567807+Deepanjali2915@users.noreply.github.com>
2021-09-06 12:08:30 +0530

    Add files via upload

---
+++ prime no.py
@@ -0,0 +1,14 @@
+# i=int(input("Enter the number:"))
+# if i%1==0 and i%i==0 and i%2!=0 and i%3!=0:
+#     print("it is a prime no.:")
+# else:
+#     print("not a prime number.:")    
+
+
+
+
+# i=int(input("Enter the number:"))
+# if i%3==0 and i%400==0:
+#     print("prime number:")
+# else:
+#     print("not")
