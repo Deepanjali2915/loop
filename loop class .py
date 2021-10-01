03586e00305c8419192b83f8c08de6a757765d5d
Deepanjali <89567807+Deepanjali2915@users.noreply.github.com>
2021-10-01 14:17:24 +0530

    Add files via upload

---
+++ loop class .py
@@ -0,0 +1,8 @@
+num=int(input("Enter the number:"))
+i=0
+while i<=num:
+    if i%2==0:
+        print(i,"It is a even number:")
+    else:
+        print(i,"IT is a odd number:")
+    i=i+1        
